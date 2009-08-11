1b6872e561e94bc86471426c1d0ea12c007c6999
loizides <>
2009-08-11 10:50:38 +0000

    Cleanup

--- Mods/interface/PDFProducer.h
+++ Mods/interface/PDFProducer.h
@@ -1,34 +1,33 @@
 //--------------------------------------------------------------------------------------------------
-// $Id: PDFProducer.h,v 1.2 2009/04/07 15:37:09 loizides Exp $
+// $Id: PDFProducer.h,v 1.1 2009/08/10 20:27:47 ceballos Exp $
 //
 // PDFProducer
 //
-// Computes PDFs
+// Computes PDFs from LHAPDF based on input from MCEventInfo.
 //
-// Authors: G. Gomez-Ceballos
+// Authors: G.Gomez-Ceballos
 //--------------------------------------------------------------------------------------------------
 
 #ifndef MITPHYSICS_MODS_PDFProducer_H
 #define MITPHYSICS_MODS_PDFProducer_H
 
 #include "MitAna/TreeMod/interface/BaseMod.h" 
-#include "MitAna/DataTree/interface/MCEventInfo.h"
 
 class TH1D;
 class TH2D;
 
 namespace mithep 
 {
+  class MCEventInfo;
   class PDFProducer : public BaseMod
   {
     public:
-      PDFProducer(const char *name="PDFProducer", 
-                   const char *title="PDF information module");
-      ~PDFProducer();
+      PDFProducer(const char *name="PDFProducerMod", 
+                  const char *title="PDF producer module");
 
-      void           SetPrintDebug(bool b)             { fPrintDebug = b;    }
-      void           SetMCEventInfoName(const char *s) { fMCEvInfoName  = s; }
-      void           SetPDFName(const char *s)         { fPDFName = s;       }
+      void               SetMCEventInfoName(const char *s)   { fMCEvInfoName = s; }
+      void               SetPDFName(const char *s)           { fPDFName      = s; }
+      void               SetPrintDebug(Bool_t b)             { fPrintDebug   = b; }
 
     protected:
       void               Process();
@@ -36,8 +35,8 @@ namespace mithep
 
       Bool_t             fPrintDebug;       //=true then print debug info
       TString            fMCEvInfoName;     //event info branch name
-      const MCEventInfo *fMCEventInfo;      //!event info branch pointer
       TString            fPDFName;          //PDF name
+      const MCEventInfo *fMCEventInfo;      //!event info branch pointer
       TH1D              *hDPDFHisto[10];    //!output histograms
 
     ClassDef(PDFProducer, 1) // Module to produce PDFs
