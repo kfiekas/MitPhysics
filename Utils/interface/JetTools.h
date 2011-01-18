5c6b141f824826df5838474bc5b1b4d191073e25
ceballos <>
2011-01-18 16:48:23 +0000

    new

--- Utils/interface/JetTools.h
+++ Utils/interface/JetTools.h
@@ -1,11 +1,3 @@
-//-------------------
-//
-// Jet Tools
-//
-// S Markson
-//
-//-------------------
-
 #ifndef MITPHYSICS_UTILS_JETTOOLS_H
 #define MITPHYSICS_UTILS_JETTOOLS_H
 
@@ -15,8 +7,7 @@
 #include "MitAna/DataTree/interface/ParticleCol.h"
 #include "MitAna/DataTree/interface/PFCandidateCol.h"
 #include "MitAna/DataTree/interface/JetCol.h"
-#include "MitAna/DataTree/interface/Jet.h"
-#include "MitAna/DataTree/interface/PFJet.h"
+#include "MitAna/DataTree/interface/PFJetCol.h"
 #include "MitPhysics/Init/interface/ModNames.h"
 #include "MitAna/DataCont/interface/ObjArray.h"
 #include "MitAna/DataTree/interface/TrackCol.h"
@@ -28,22 +19,24 @@
 
 namespace mithep {
   class JetTools {
-    public:
-      JetTools();
-      virtual ~JetTools();
-
-      static Double_t NJettiness(const ParticleOArr *particles, const JetOArr *jets, bool UseQ = kFALSE, double Y = 0.0);
-      static Double_t NJettiness(const TrackOArr *tracks, const JetOArr *jets, bool UseQ = kFALSE, double Y = 0.0);
-      static Double_t NJettiness(const JetOArr *jetsS, const JetOArr *jets, bool UseQ = kFALSE, double Y = 0.0);
-      static Double_t NJettiness(const CaloTowerOArr *calos, const JetOArr *jets, bool UseQ = kFALSE, double Y = 0.0);
-      static Double_t M_r(const ParticleOArr *particles);
-      static Double_t Beta_r(const ParticleOArr *particles);
-      static Double_t M_r_t(const ParticleOArr *particles, const Met *met);
-      static Double_t Razor(const ParticleOArr *particles, const Met *met);
-      static Double_t CosineOmega(const Particle *particles0, const Particle *particles1);
-      static Double_t MtHiggs(const ParticleOArr *leptons, const Met *met, double metFraction[2], int nsel);
-      static void     Alpha(Double_t AlphaVar[2], const TrackCol *tracks, Jet *jet, const VertexCol *vertices, Double_t  delta_z, Double_t delta_cone);
-      static void     Alpha(Double_t AlphaVar[2], const PFJet *jet, const VertexCol *vertices, Double_t  delta_z);
+  public:
+    JetTools();
+    virtual ~JetTools();
+    
+    static Double_t NJettiness(const ParticleOArr *particles, const JetOArr *jets, double Q = 1, double Y = 0.0);
+    static Double_t NJettiness(const PFCandidateOArr *particles, const JetOArr *jets, double Q = 1, double Y = 0.0);
+    static Double_t NJettiness(const TrackOArr *tracks, const JetOArr *jets, double Q = 1, double Y = 0.0);
+    static Double_t NJettiness(const JetOArr *jetsS, const JetOArr *jets, double Q = 1, double Y = 0.0);
+    static Double_t NJettiness(const CaloTowerOArr *calos, const JetOArr *jets, double Q = 1, double Y = 0.0);
+    
+    static Double_t M_r(const ParticleOArr *particles);
+    static Double_t Beta_r(const ParticleOArr *particles);
+    static Double_t M_r_t(const ParticleOArr *particles, const Met *met);
+    static Double_t Razor(const ParticleOArr *particles, const Met *met);
+    static Double_t CosineOmega(const Particle *particles0, const Particle *particles1);
+    static Double_t MtHiggs(const ParticleOArr *leptons, const Met *met, double metFraction[2], int nsel);
+    static void     Alpha(Double_t AlphaVar[2], const TrackCol *tracks, Jet *jet, const VertexCol *vertices, Double_t  delta_z, Double_t delta_cone);
+    static void     Alpha(Double_t AlphaVar[2], const PFJet *jet, const VertexCol *vertices, Double_t  delta_z);
     
     ClassDef(JetTools, 0)
   };
