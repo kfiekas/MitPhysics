089e50c6de165ad328d584eef922f0e091f146cc
dkralph <>
2011-03-09 14:33:39 +0000

    Added hps options; removed non-hps criteria from hps selection path.

--- Mods/src/PFTauIDMod.cc
+++ Mods/src/PFTauIDMod.cc
@@ -1,4 +1,4 @@
-// $Id: PFTauIDMod.cc,v 1.2 2010/08/19 14:37:17 ceballos Exp $
+// $Id: PFTauIDMod.cc,v 1.3 2011/02/17 14:08:49 ceballos Exp $
 
 #include "MitPhysics/Mods/interface/PFTauIDMod.h"
 #include "MitPhysics/Init/interface/ModNames.h"
@@ -20,6 +20,7 @@ PFTauIDMod::PFTauIDMod(const char *name, const char *title) :
   fSignalMassMin(0.13),
   fSignalMassMax(2.00),
   fIsHPSSel(kFALSE),
+  fHPSIso("loose"),
   fPFTaus(0)
 {
   // Constructor.
@@ -35,34 +36,34 @@ void PFTauIDMod::Process()
   PFTauOArr *GoodTaus = new PFTauOArr;
   GoodTaus->SetName(fGoodPFTausName);
 
-  for (UInt_t i=0; i<fPFTaus->GetEntries(); ++i) {    
-    const PFTau *tau = fPFTaus->At(i);        
+  for (UInt_t i=0; i<fPFTaus->GetEntries(); ++i) {
+    const PFTau *tau = fPFTaus->At(i);
 
-    if (tau->NSignalPFCands() == 0)
-      continue;
+    if(fIsHPSSel == kFALSE) {
+      if (tau->NSignalPFCands() == 0)
+	continue;
 
-    CompositeParticle tauSystem;
-    CompositeParticle tauChargedSystem;
-    UInt_t nTrk = 0;
-    for (UInt_t j=0; j<tau->NSignalPFCands(); ++j) {
-      tauSystem.AddDaughter(tau->SignalPFCand(j));
-      if(tau->SignalPFCand(j) != 0 &&
-         tau->SignalPFCand(j)->Charge() != 0){
-        nTrk++;
-        tauChargedSystem.AddDaughter(tau->SignalPFCand(j));
+      CompositeParticle tauSystem;
+      CompositeParticle tauChargedSystem;
+      UInt_t nTrk = 0;
+      for (UInt_t j=0; j<tau->NSignalPFCands(); ++j) {
+	tauSystem.AddDaughter(tau->SignalPFCand(j));
+	if(tau->SignalPFCand(j) != 0 &&
+	   tau->SignalPFCand(j)->Charge() != 0){
+	  nTrk++;
+	  tauChargedSystem.AddDaughter(tau->SignalPFCand(j));
+	}
       }
-    }
 
-    if (nTrk != 1 && nTrk != 3)
-      continue;
+      if (nTrk != 1 && nTrk != 3)
+	continue;
 
-    if (TMath::Abs(tau->Charge()) != 1)
-      continue;
+      if (TMath::Abs(tau->Charge()) - 1.0 > 0.0001)
+	continue;
 
-    if (tauSystem.Pt() <= fPtMin)
-      continue;
+      if (tauSystem.Pt() <= fPtMin)
+	continue;
 
-    if (fIsHPSSel == kFALSE){
       if(!tau->LeadChargedHadronPFCand())
 	continue;
 
@@ -78,22 +79,36 @@ void PFTauIDMod::Process()
       if (tauChargedSystem.Mass() <= fSignalMassMin || tauChargedSystem.Mass() >= fSignalMassMax)
 	continue;
     }
-    else {
-      if(tau->DiscriminationAgainstElectron() == 0)
+    else { // if we're doing hps selection:
+      if(tau->Pt() <= fPtMin)
+	continue;
+      if(tau->DiscriminationByDecayModeFinding() < 0.5)
         continue;
-
-      if(tau->DiscriminationAgainstMuon() == 0)
+      if(tau->DiscriminationAgainstElectron() < 0.5)
         continue;
-
-      if(tau->DiscriminationByDecayModeFinding() == 0)
+      if(tau->DiscriminationAgainstMuon() < 0.5)
         continue;
-
-      if(tau->DiscriminationByMediumIsolation() == 0)
-        continue;      
+      // "loose" should be default, but others can be used
+      if(fHPSIso.Contains("loose",TString::kIgnoreCase)) {
+	if(tau->DiscriminationByLooseIsolation() < 0.5)
+	  continue;
+      }
+      else if(fHPSIso.Contains("med",TString::kIgnoreCase)) {
+	if(tau->DiscriminationByMediumIsolation() < 0.5)
+	  continue;
+      }
+      else if(fHPSIso.Contains("tight",TString::kIgnoreCase)) {
+	if(tau->DiscriminationByTightIsolation() < 0.5)
+	  continue;
+      }
+      else {
+	SendError(kWarning,"Process","ERROR: HPS Isolation not properly defined!");
+      }
     }
 
     // add good tau to output collection
     GoodTaus->Add(tau);
+    
   }
 
   // sort according to pt
@@ -108,6 +123,11 @@ void PFTauIDMod::SlaveBegin()
 {
   // Run startup code on the computer (slave) doing the actual analysis. Here,
   // we just request the Tau collection branch.
+  if(fIsHPSSel) {
+    fPFTausName = TString(Names::gkHPSTauBrn);
+  }
+  else
+    fPFTausName = TString(Names::gkPFTauBrn);
 
   ReqBranch(fPFTausName, fPFTaus);
 }
