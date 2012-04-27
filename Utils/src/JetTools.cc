415595650881393b706f1ee3b7af68ed2430702a
ceballos <>
2012-04-27 22:44:12 +0000

    minor fix

--- Utils/src/JetTools.cc
+++ Utils/src/JetTools.cc
@@ -547,10 +547,10 @@ Bool_t  JetTools::passPFLooseId(const PFJet *iJet) {
   if(iJet->RawMom().E()                              == 0)       return false;
   if(iJet->NeutralHadronEnergy()/iJet->RawMom().E()  >  0.99)    return false;
   if(iJet->NeutralEmEnergy()/iJet->RawMom().E()      >  0.99)    return false;
-  if(iJet->NConstituents()                  <  2)	return false;
+  if(iJet->NConstituents()                           <  2)	 return false;
   if(iJet->ChargedHadronEnergy()/iJet->RawMom().E()  <= 0     && fabs(iJet->Eta()) < 2.4 ) return false;
   if(iJet->ChargedEmEnergy()/iJet->RawMom().E()      >  0.99  && fabs(iJet->Eta()) < 2.4 ) return false;
-  if(iJet->ChargedMultiplicity()            < 1      && fabs(iJet->Eta()) < 2.4 ) return false;
+  if(iJet->ChargedMultiplicity()                     < 1      && fabs(iJet->Eta()) < 2.4 ) return false;
   return true;
 }
 
