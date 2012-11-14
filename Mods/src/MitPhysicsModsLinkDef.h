316cbce5fb1ba2f545ec60b92f3c9127d8c9ec84
sixie <>
2012-11-14 20:05:37 +0000

    move HZG stuff to MitHZGamma

--- Mods/src/MitPhysicsModsLinkDef.h
+++ Mods/src/MitPhysicsModsLinkDef.h
@@ -1,4 +1,4 @@
-// $Id: MitPhysicsModsLinkDef.h,v 1.36 2012/06/06 15:12:12 mingyang Exp $
+// $Id: MitPhysicsModsLinkDef.h,v 1.37 2012/07/24 09:53:45 fabstoec Exp $
 
 #ifndef MITPHYSICS_MODS_LINKDEF_H
 #define MITPHYSICS_MODS_LINKDEF_H
@@ -32,7 +32,6 @@
 #include "MitPhysics/Mods/interface/TrackingPurityFilterMod.h"
 #include "MitPhysics/Mods/interface/MVASystematicsMod.h"
 #include "MitPhysics/Mods/interface/SeparatePileUpMod.h"
-#include "MitPhysics/Mods/interface/LeptonPairPhotonTreeWriter.h"
 #endif
 
 #ifdef __CINT__
@@ -79,6 +78,4 @@
 #pragma link C++ class mithep::TrackingPurityFilterMod+;
 #pragma link C++ class mithep::MVASystematicsMod+;
 #pragma link C++ class mithep::SeparatePileUpMod+;
-#pragma link C++ class mithep::LeptonPairPhotonTreeWriter+;
-#pragma link C++ class mithep::LeptonPairPhotonEvent+;
 #endif
