d2a5e753c4ed28e798d8f149b78610dd64120a71
sixie <>
2012-12-12 16:33:39 +0000

    update rochestor corrections to V4.1. add 2012 version.

--- Utils/interface/rochcor2012.h
+++ Utils/interface/rochcor2012.h
@@ -11,18 +11,20 @@
 #include <TRandom3.h>
 
 
-#ifndef ROCHCOR
-#define ROCHCOR
+#ifndef ROCHCOR2012
+#define ROCHCOR2012
 
-class rochcor {
+class rochcor2012 {
  public:
-  rochcor();
-  rochcor(int seed);
-  ~rochcor();
+  rochcor2012();
+  rochcor2012(int seed);
+  ~rochcor2012();
   
   void momcor_mc(TLorentzVector&, float, float, int, float&);
   void momcor_data(TLorentzVector&, float, float, int, float&);
   
+  void musclefit_data(TLorentzVector& , TLorentzVector&);
+
   float zptcor(float);
   int etabin(float);
   int phibin(float);
@@ -36,36 +38,22 @@ class rochcor {
 
   //---------------------------------------------------------------------------------------------
   
-  static const float dcor_bfA[8][8];  
-  static const float dcor_maA[8][8];
-  static const float mcor_bfA[8][8];
-  static const float mcor_maA[8][8];
-  static const float dcor_bfAer[8][8];  
-  static const float dcor_maAer[8][8];
-  static const float mcor_bfAer[8][8];
-  static const float mcor_maAer[8][8];
-
-  static const float dcor_bfB[8][8];  
-  static const float dcor_maB[8][8];
-  static const float mcor_bfB[8][8];
-  static const float mcor_maB[8][8];
-  static const float dcor_bfBer[8][8];  
-  static const float dcor_maBer[8][8];
-  static const float mcor_bfBer[8][8];
-  static const float mcor_maBer[8][8];
+  static const float dcor_bf[8][8];  
+  static const float dcor_ma[8][8];
+  static const float mcor_bf[8][8];
+  static const float mcor_ma[8][8];
+  static const float dcor_bfer[8][8];  
+  static const float dcor_maer[8][8];
+  static const float mcor_bfer[8][8];
+  static const float mcor_maer[8][8];
 
   //=======================================================================================================
   
-  static const float dmavgA[8][8];  
-  static const float dpavgA[8][8];  
-  static const float mmavgA[8][8];  
-  static const float mpavgA[8][8];
+  static const float dmavg[8][8];  
+  static const float dpavg[8][8];  
+  static const float mmavg[8][8];  
+  static const float mpavg[8][8];
 
-  static const float dmavgB[8][8];  
-  static const float dpavgB[8][8];  
-  static const float mmavgB[8][8];  
-  static const float mpavgB[8][8];
-  
   //===============================================================================================
   //parameters for Z pt correction
   static const int nptbins=84;
