b9ce3889bd983ddc0ec5d525e8a6cb91fd1f2b93
dimatteo <>
2013-10-09 22:16:27 +0000

    new cleaning

--- Mods/src/CosmicCleaningMod.cc
+++ Mods/src/CosmicCleaningMod.cc
@@ -32,12 +32,14 @@ void CosmicCleaningMod::Process()
 
   // Go through all cosmics and remove cosmic overlaps with collision muons and duplicates.
   std::vector<const Muon*> CleanCosTemp;
-
+  
   if (fCosmics) {
     for (UInt_t i=0; i<fCosmics->GetEntries(); ++i) {    
       const Muon *u = fCosmics->At(i);   
 
       FourVectorM mom(u->Mom());
+      // Invert the cosmic momentum for fake cosmic cleaning
+      FourVectorM momInv(mom.Pt(),-mom.Eta(),mom.Phi()+TMath::Pi(),mom.M());
 
       // Check whether it overlaps with a good muon: If the muon and cosmic both have
       // tracker tracks then compare the tracks, otherwise
@@ -46,7 +48,8 @@ void CosmicCleaningMod::Process()
         UInt_t n = CleanMuons->GetEntries();
         for (UInt_t j=0; j<n; ++j) {
           Double_t deltaR = MathUtils::DeltaR(CleanMuons->At(j)->Mom(), mom);     
-          if (deltaR < fDeltaR) {
+          Double_t deltaRInv = MathUtils::DeltaR(CleanMuons->At(j)->Mom(), momInv);     
+          if (deltaR < fDeltaR || deltaRInv < fDeltaR) {
             isMuonOverlap = kTRUE;
             break;         
           }
@@ -58,6 +61,19 @@ void CosmicCleaningMod::Process()
       if (isMuonOverlap)
         continue;
 
+      // Check whether it overlaps with another cosmic candidate: 	 
+      bool isCosmicOverlap = kFALSE; 	 
+      for (UInt_t j=0; j<CleanCosTemp.size(); ++j) { 	 
+        Double_t deltaR = MathUtils::DeltaR(CleanCosTemp[j]->Mom(), mom); 	 
+        if (deltaR < 0.1) {	 
+          isCosmicOverlap = kTRUE; 	 
+          break;
+        }         
+      } 	 
+     
+      if (isCosmicOverlap) 	 
+        continue;
+        
       // if no overlaps then add to clean cosmics
       CleanCosTemp.push_back(fCosmics->At(i));   
     } 
